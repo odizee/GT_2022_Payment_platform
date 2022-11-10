e6e5e56fb0780609e6cb36eaa1df4c31216663af
drew-chidi <chidi.andrew@gmail.com>
2022-11-10 15:05:35 +0100

    added navbar to all screens

--- client/src/components/OtpContent.js
+++ client/src/components/OtpContent.js
@@ -1,54 +1,59 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import styles from "./Styles/Forgotpassword.module.css";
 import bg_image from "../assets/undraw_secure_login_pdn4 2.svg";
-import PasswordStrengthBar from "react-password-strength-bar";
+import NavBar from "./NavBar";
 
 function OtpContent() {
   const [password, setPassword] = useState();
   const [passwordScore, setPasswordScore] = useState();
 
   return (
-    <div className={styles.forgotpassword}>
-      <div className={styles.form}>
-        <div className={styles.form_container}>
-          <div className={styles.formhead}>
-            <h2>Verify account</h2>
-            <h3>Verify your account</h3>
-          </div>
+    <Fragment>
+      <NavBar />
+      <div className={styles.forgotpassword}>
+        <div className={styles.form}>
+          <div className={styles.form_container}>
+            <div className={styles.formhead}>
+              <h2>Verify account</h2>
+              <h3>Verify your account</h3>
+            </div>
 
-          <form>
-            <div className={styles.input_group}>
-              <label>Email</label>
-              <div className={styles.input_otp}>
-                <input type="email" placeholder="anderson@gmail.com" />
-                <button style={{color: "white", background: "#01A8F8"}}>Send OTP</button>
+            <form>
+              <div className={styles.input_group}>
+                <label>Email</label>
+                <div className={styles.input_otp}>
+                  <input type='email' placeholder='anderson@gmail.com' />
+                  <button style={{ color: "white", background: "#01A8F8" }}>
+                    Send OTP
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className={styles.input_group}>
-              <label>OTP</label>
+              <div className={styles.input_group}>
+                <label>OTP</label>
+                <input
+                  type='password'
+                  placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;'
+                />
+              </div>
+
               <input
-                type="password"
-                placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
+                type='submit'
+                value='Continue'
+                className={styles.pass_submit}
               />
-            </div>
-            
-            <input
-              type="submit"
-              value="Continue"
-              className={styles.pass_submit}
-            />
-          </form>
+            </form>
+          </div>
+        </div>
+        <div className={styles.bg_image}>
+          <img src={bg_image} alt='' />
+          <div className={styles.bg_image__text}>
+            <h2>Welcome to your dashboard</h2>
+            <h3>Signin to explore new features</h3>
+          </div>
         </div>
       </div>
-      <div className={styles.bg_image}>
-        <img src={bg_image} alt="" />
-        <div className={styles.bg_image__text}>
-          <h2>Welcome to your dashboard</h2>
-          <h3>Signin to explore new features</h3>
-        </div>
-      </div>
-    </div>
+    </Fragment>
   );
-};
+}
 
 export default OtpContent;
